21b65d0214e1e96870cfa8e59b0296e8639a7385
Daniel Fuentes <daniel.richard.fuentes@gmail.com>
2021-02-01 16:17:59 -0800

    Added some basic physics
    Reed's request: "Make george jump and run!"

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/dfuentes/reedgame
 go 1.15
 
 require (
-	github.com/hajimehoshi/ebiten v1.12.7
+	github.com/SolarLune/resolv v0.0.0-20201206065135-f22ef61a44a3
 	github.com/hajimehoshi/ebiten/v2 v2.0.4
-	golang.org/x/tools/gopls v0.6.4 // indirect
+	golang.org/x/sys v0.0.0-20210119212857-b64e53b001e4 // indirect
 )
--- main.go
+++ main.go
@@ -2,15 +2,28 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/color"
 	"image/png"
 	"os"
 
+	"github.com/SolarLune/resolv"
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
 const (
-	ScreenX = 320
-	ScreenY = 240
+	ScreenX = 640
+	ScreenY = 480
+
+	SpriteTileX = 48
+	SpriteTileY = 48
+
+	PlayerAccel     = 5.0
+	PlayerJumpSpeed = 8.0
+	Gravity         = 15.0
+	Friction        = 0.25
+	MaxSpeed        = 3
 )
 
 type GameState int
@@ -22,33 +35,90 @@ const (
 
 type Sprite struct {
 	image *ebiten.Image
-	posX  float64
-	posY  float64
 }
 
+type Player struct {
+	sprite   Sprite
+	velX     float64
+	velY     float64
+	rect     *resolv.Rectangle
+	grounded bool
+}
 type Game struct {
 	state  GameState
-	player Sprite
+	player Player
+	space  *resolv.Space
+}
+
+func GetTileRect(tilex, tiley int) image.Rectangle {
+	return image.Rect(tilex*SpriteTileX,
+		tiley*SpriteTileY,
+		(tilex+1)*SpriteTileX,
+		(tiley+1)*SpriteTileY)
 }
 
 func (g *Game) Update() error {
-	// Handle input
-	if ebiten.IsKeyPressed(ebiten.KeyUp) {
-		g.player.posY -= 1.0
-	}
-	if ebiten.IsKeyPressed(ebiten.KeyDown) {
-		g.player.posY += 1.0
-	}
+	delta := 1.0 / float64(ebiten.MaxTPS())
+
 	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
-		g.player.posX -= 1.0
+		g.player.velX -= PlayerAccel*delta + Friction
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyRight) {
-		g.player.posX += 1.0
+		g.player.velX += PlayerAccel*delta + Friction
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyQ) {
 		g.state = Quitting
 	}
 
+	if res := g.space.Resolve(g.player.rect, 0, 3); res.Colliding() {
+		g.player.velY = 0
+		g.player.grounded = true
+	} else {
+		g.player.grounded = false
+	}
+
+	if g.player.grounded && (ebiten.IsKeyPressed(ebiten.KeySpace) || ebiten.IsKeyPressed(ebiten.KeyUp)) {
+		g.player.velY -= PlayerJumpSpeed
+	}
+
+	if res := g.space.Resolve(g.player.rect, int32(g.player.velX), 0); res.Colliding() {
+		fmt.Println("collide x")
+		g.player.rect.X += res.ResolveX
+		g.player.velX = 0
+	}
+
+	if res := g.space.Resolve(g.player.rect, 0, int32(g.player.velY)); res.Colliding() {
+		fmt.Println("collide y")
+		g.player.rect.Y += res.ResolveY
+		g.player.velY = 0
+	}
+	g.player.velY += delta * Gravity
+
+	if g.player.velX > Friction {
+		g.player.velX -= Friction
+	} else if g.player.velX < -Friction {
+		g.player.velX += Friction
+	} else {
+		g.player.velX = 0
+	}
+
+	if g.player.velX > MaxSpeed {
+		g.player.velX = MaxSpeed
+	}
+	if g.player.velX < -MaxSpeed {
+		g.player.velX = -MaxSpeed
+	}
+
+	g.player.rect.X += int32(g.player.velX)
+	g.player.rect.Y += int32(g.player.velY)
+
+	// wrapping
+	if g.player.rect.X > ScreenX {
+		g.player.rect.X = 0
+	}
+	if g.player.rect.X < 0 {
+		g.player.rect.X = ScreenX
+	}
 	switch g.state {
 	case Playing:
 	case Quitting:
@@ -61,9 +131,12 @@ func (g *Game) Update() error {
 func (g *Game) Draw(screen *ebiten.Image) {
 	op := &ebiten.DrawImageOptions{}
 
-	op.GeoM.Scale(0.25, 0.25)
-	op.GeoM.Translate(g.player.posX, g.player.posY)
-	screen.DrawImage(g.player.image, op)
+	// draw floor
+	ebitenutil.DrawRect(screen, 0, ScreenY-10, ScreenX, 10, color.White)
+
+	// ebitenutil.DrawRect(screen, 0, 0, ScreenX, ScreenY, color.White)
+	op.GeoM.Translate(float64(g.player.rect.X), float64(g.player.rect.Y))
+	screen.DrawImage(g.player.sprite.image, op)
 
 }
 
@@ -73,22 +146,30 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 
 func main() {
 
-	gfd, err := os.Open("./assets/george.png")
+	spritesfd, err := os.Open("./assets/monkeysprites.png")
 	if err != nil {
 		panic(err)
 	}
-	gim, err := png.Decode(gfd)
+	spritesImage, err := png.Decode(spritesfd)
 	if err != nil {
 		panic(err)
 	}
 
-	geim := ebiten.NewImageFromImage(gim)
+	spritesEbitenImage := ebiten.NewImageFromImage(spritesImage)
 
-	gsprite := Sprite{
-		image: geim,
-		posX:  float64(ScreenX / 2),
-		posY:  float64(ScreenY / 2),
+	player := Player{
+		sprite: Sprite{
+			image: spritesEbitenImage.SubImage(GetTileRect(0, 2)).(*ebiten.Image),
+		},
+		rect: resolv.NewRectangle(ScreenX/2, ScreenY/2, SpriteTileX, SpriteTileY),
+		velX: float64(0),
+		velY: float64(0),
 	}
+
+	space := resolv.NewSpace()
+
+	floor := resolv.NewRectangle(0, ScreenY-10, ScreenX, 10)
+	space.Add(floor)
 
 	fmt.Println("starting")
 	ebiten.SetWindowSize(2*ScreenX, 2*ScreenY)
@@ -96,7 +177,8 @@ func main() {
 
 	game := &Game{
 		state:  Playing,
-		player: gsprite,
+		player: player,
+		space:  space,
 	}
 	if err := ebiten.RunGame(game); err != nil {
 		panic(err)
